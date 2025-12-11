c96d80303a753b2ef3f1ba64da69b3dc6cc5f8fc
T-Jayaprakash <jayaprakash.5388@gmail.com>
2025-12-11 11:53:57 +0530

    Critical Fix: Update version constants check URL to root version.json

--- constants/version.ts
+++ constants/version.ts
@@ -1,9 +1,9 @@
 // App version management
 // Update this version number whenever you release a new version
-export const APP_VERSION = '2.8.0';
+export const APP_VERSION = '2.9.0';
 
 // Minimum supported version - users below this will be forced to update
 export const MIN_SUPPORTED_VERSION = '2.8.0';
 
 // Remote config URL where the latest version info is fetched from
-export const VERSION_CHECK_URL = 'https://raw.githubusercontent.com/T-Jayaprakash/LastBench/main/api/version.json';
+export const VERSION_CHECK_URL = 'https://raw.githubusercontent.com/T-Jayaprakash/LastBench/main/version.json';
