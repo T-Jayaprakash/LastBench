681a69c778ad4ae3496ab84a0cefd429cc527002
T-Jayaprakash <jayaprakash.5388@gmail.com>
2026-02-03 19:53:31 +0530

    Critical Fix: Updated Application ID to com.jayple.app to match google-services.json and enable push notifications

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lastbench.app',
+  appId: 'com.jayple.app',
   appName: 'Genfess',
   webDir: 'dist',
   plugins: {
